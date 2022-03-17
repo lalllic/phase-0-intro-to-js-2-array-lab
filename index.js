3c449965d313e81865d761769c32c5ffce4383ea
Isabella Costabile <costabileisa@gmail.com>
2022-03-17 14:49:41 -0400

    commit tree a42432acb13afc5bf729aa950e713d07a132bb60

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function reset() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+reset();
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+reset();
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+reset();
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+reset();
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = [...cats.slice(0, -1)];
+    return newCats;
+}
+
+function removeFirstCat() {
+    let newCats = [...cats.slice(1)];
+    return newCats;
+}
